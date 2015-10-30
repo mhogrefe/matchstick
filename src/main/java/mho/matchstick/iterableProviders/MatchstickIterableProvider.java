d65f935878d47e0825055c4ca1462a2f30e58396
mhogrefe <mikhailhogrefe@gmail.com>
2015-10-29 23:02:15 -0400

    fixed method decls

--- src/main/java/mho/matchstick/iterableProviders/MatchstickIterableProvider.java
+++ src/main/java/mho/matchstick/iterableProviders/MatchstickIterableProvider.java
@@ -2317,7 +2317,7 @@ public abstract strictfp class MatchstickIterableProvider {
      * @param xs a {@code List} of elements
      * @param <T> the type of values in the {@code List}s
      */
-    public @NotNull <T> Iterable<List<T>> subsetsLexAtLeast(int minSize, @NotNull List<T> xs) {
+    public @NotNull <T extends Comparable<T>> Iterable<List<T>> subsetsLexAtLeast(int minSize, @NotNull List<T> xs) {
         return wheelsProvider.subsetsLexAtLeast(minSize, xs);
     }
 
@@ -2332,7 +2332,7 @@ public abstract strictfp class MatchstickIterableProvider {
         return wheelsProvider.stringSubsetsLexAtLeast(minSize, s);
     }
 
-    public @NotNull <T> Iterable<List<T>> subsets(int size, @NotNull Iterable<T> xs) {
+    public @NotNull <T extends Comparable<T>> Iterable<List<T>> subsets(int size, @NotNull Iterable<T> xs) {
         return wheelsProvider.subsets(size, xs);
     }
 
